d0f290eafba1498cb5e8fa88dad2b0728a635401
Kangjik Kim <rkqqkb@gmail.com>
2024-06-23 23:27:54 +0900

    목록과 상세 뷰 만들기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.http import Http404
+
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404('No Post Found')
+    return render(request, 'blog/post/detail.html', {'post': post})
